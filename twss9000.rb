59a81958ece8fcf20fd8e817246ea7a00f7b1609
bengarvey <ben@bengarvey.com>
2013-07-02 07:31:16 -0400

    Working prototype

--- twss9000.rb
+++ twss9000.rb
@@ -18,22 +18,40 @@ Twitter.configure do |config|
   config.consumer_key       = conf['consumer_key']
   config.consumer_secret    = conf['consumer_secret']
   config.oauth_token        = conf['oauth_token']
-  config.oauth_token_secret = conf['otath_token_secret']
+  config.oauth_token_secret = conf['oauth_token_secret']
 end
+
+#Twitter.update('It can only be attributable to human error')
+
 
 parentIds = Array.new
 searchText = "#twss"
 
-Twitter.search(searchText, :count => 20, :result_type => "recent").results.map do |status|
-  parentIds.push(status.in_reply_to_status_id)
+Twitter.search(searchText, :count => 50, :result_type => "recent").results.map do |status|
+  tweet     = {}
+    tweet['id']     = status.in_reply_to_status_id  
+    tweet['userid'] = status.in_reply_to_user_id
+    parentIds.push(tweet)
 end
 
-parentIds.each do |id|
-  if !id.nil?
-    status = Twitter.status(id)
-    puts "\n#{status.from_user}: #{status.text}"
+parentIds.each do |tweet|
+  if !tweet['id'].nil? and !tweet['userid'].nil?
+    user    = Twitter.user(tweet['userid'])
+    if !user.protected
+      status  = Twitter.status(tweet['id'])
+      if /.*twss.*/i !~ status.text
+        puts "\n#{status.from_user}: #{status.text}"
+        puts "RT? y/n"
+        answer = gets()
+        if answer == "y\n"
+          Twitter.retweet(tweet['id'])
+          puts "RT'd!"
+        else
+          puts "Skipped"
+        end
+      end
+    end
   end
 end
-
 
 
